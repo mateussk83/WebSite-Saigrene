e20aa94e028e0ba4d12b4bde6a9385b497f326e1
Mateus Garcia <mateussk83@gmail.com>
2022-08-23 11:53:45 -0300

    fix: bug responsive services

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,9 +2,7 @@ import { WhatsappLogo } from 'phosphor-react'
 
 export function Home() {
   return (
-    <section id="home" className="bg-blue-300  justify-center  grid grid-flow-col">
-      <div className="pt-20 pl-[7.75rem] " id="wrapper">
-    <section id="home" className="bg-blue-300  justify-center flex items-center">
+     <section id="home" className="bg-blue-300  justify-center flex items-center">
       <div className="pt-20 flex justify-center" id="wrapper">
         <header className="">
           <h4
@@ -23,7 +21,6 @@ export function Home() {
             </p>
             <a
               href="#"
-              className="bg-blue-700 uppercase font-bold gap-[0.75rem] text-white rounded-full text-sm px-2 py-[1rem] flex justify-center w-[250px] items-center mt-8">
               className="bg-blue-700 uppercase font-bold gap-[0.75rem] text-white rounded-full text-sm py-[1rem] flex justify-center w-[250px] items-center mt-8">
               <WhatsappLogo size={25} weight="light"/>
               Agende sua Consulta
@@ -32,11 +29,9 @@ export function Home() {
         </header>
       </div>
 
-      <div id="col-b" className="">
-      <div id="col-b">
+       <div id="col-b">
         <img
-          className="block left-0 w-[27rem] rigth-[18.75rem] max-w-[100%] mr-[5.2rem]"
-          className="block w-[27rem] max-w-[100%] "
+         className="block w-[27rem] max-w-[100%] "
           src="./src/assets/mamae.png"
           alt="Erica Luciana Garcia"
         />
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -7,9 +7,10 @@ export function Services() {
         <h4 className="text-[0.875rem] leading-snug text-blue-700 tracking-wide font-bold pt-[8rem]">SERVIÇOS</h4>
         <h1 className="text-gray-900 font-bold mt-3 text-[2.5rem] w-[37.5rem] leading-[52.08px] text-center" >Como podemos ajudá-lo a se sentir melhor?</h1>
       </header>
-      <div id="cards" className="grid grid-cols-3 text-left mt-[4rem]">
+      <div id="cards" className="flex text-left justify-center gap-10 mt-[4rem]">
+        <div id="col-1" className="flex flex-col gap-10">
         <div
-          className=" w-[347px] h-[207px] ml-[7.75rem] border border-solid rounded-md bg-white p-6">
+          className=" w-[347px] h-[207px] border border-solid rounded-md bg-white p-6">
           <CheckCircle size={24} color="#6848c8" weight="duotone" />
 
           <h3 className="text-gray-900 font-bold mt-4">Terapia de casal</h3>
@@ -20,7 +21,7 @@ export function Services() {
         </div>
 
         <div
-          className="w-[347px] h-[207px] border border-solid rounded-md bg-white ml-[3.75rem] mr-[2.5rem] p-6">
+          className="w-[347px] h-[207px] border border-solid rounded-md bg-white p-6">
           <CheckCircle size={24} color="#6848c8" weight="duotone" />
 
           <h3 className="text-gray-900 font-bold mt-4">Terapia de casal</h3>
@@ -29,9 +30,10 @@ export function Services() {
             amet sint. Velit officia consequat duis enim.
           </p>
         </div>
-
+        </div>
+        <div id="col-2" className="flex flex-col gap-10">
         <div
-          className="  w-[347px] h-[207px] border border-solid rounded-md bg-white p-6 mr-[7.75rem]">
+          className=" w-[347px] h-[207px] border border-solid rounded-md bg-white p-6">
           <CheckCircle size={24} color="#6848c8" weight="duotone" />
 
           <h3 className="text-gray-900 font-bold mt-4">Terapia de casal</h3>
@@ -42,7 +44,19 @@ export function Services() {
         </div>
 
         <div
-          className=" mt-[2.5rem] w-[347px] h-[207px] border border-solid rounded-md bg-white p-6 ml-[7.75rem]">
+          className="w-[347px] h-[207px] border border-solid rounded-md bg-white p-6">
+          <CheckCircle size={24} color="#6848c8" weight="duotone" />
+
+          <h3 className="text-gray-900 font-bold mt-4">Terapia de casal</h3>
+          <p className="text-gray-700  mt-4 leading-snug">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+            amet sint. Velit officia consequat duis enim.
+          </p>
+        </div>
+        </div>
+        <div id="col-3" className="flex flex-col gap-10">
+        <div
+          className=" w-[347px] h-[207px] border border-solid rounded-md bg-white p-6">
           <CheckCircle size={24} color="#6848c8" weight="duotone" />
 
           <h3 className="text-gray-900 font-bold mt-4">Terapia de casal</h3>
@@ -53,27 +67,17 @@ export function Services() {
         </div>
 
         <div
-          className=" mt-[2.5rem] w-[347px] h-[207px] ml-[3.75rem] border border-solid rounded-md bg-white p-6">
+          className="w-[347px] h-[207px] border border-solid rounded-md bg-white p-6">
           <CheckCircle size={24} color="#6848c8" weight="duotone" />
 
           <h3 className="text-gray-900 font-bold mt-4">Terapia de casal</h3>
-          <p className="text-gray-700  mt-4">
+          <p className="text-gray-700  mt-4 leading-snug">
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do
             amet sint. Velit officia consequat duis enim.
           </p>
         </div>
-
-        <div
-          className=" mt-[2.5rem] w-[347px] h-[207px] border border-solid rounded-md bg-white p-6 mr-[7.75rem]" >
-          <CheckCircle size={24} color="#6848c8" weight="duotone" />
-
-          <h3 className="text-gray-900 font-bold mt-4">Terapia de casal</h3>
-          <p className="text-gray-700  mt-4">
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-            amet sint. Velit officia consequat duis enim.
-          </p>
         </div>
-      </div>
+        </div>
     </div>
   )
 } 
