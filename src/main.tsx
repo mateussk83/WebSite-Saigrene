ab5fbca23e61ffc429fca4b4944b40d983bc203d
Mateus Garcia <mateussk83@gmail.com>
2022-08-30 17:40:23 -0300

    teste deploy gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
     </BrowserRouter>
   </React.StrictMode>
